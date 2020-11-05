6e04398891863a2fc92a2b1a5260c7287661df59
Steven DUBOIS <git@duboiss.fr>
2020-11-05 13:56:35 +0100

    Add .aliases.zsh file

---
+++ .aliases.zsh
@@ -0,0 +1,14 @@
+alias compo="composer"
+alias path="echo -e ${PATH//:/\\n}" # Print each PATH entry on a separate line
+alias sym="symfony"
+alias symc="symfony console"
+
+# Docker
+alias dc="docker compose"
+alias dcu="docker-compose up"
+alias dcd="docker-compose down"
+
+# Specific windows os
+# alias bat="batcat"
+# alias code="/mnt/c/Users/Steven/AppData/Local/Programs/'Microsoft VS Code'/bin/code"
+# alias wip="ifconfig eth0 | grep 'inet' | cut -d: -f2"
