351d45a523e1a0bb001f0d4fc11202fe216ae779
Steven DUBOIS <git@duboiss.fr>
2020-11-06 15:02:34 +0100

    Update .aliases.zsh : add cp mv and mkdir alias

--- .aliases.zsh
+++ .aliases.zsh
@@ -1,12 +1,18 @@
-alias compo="composer"
 alias path="echo -e ${PATH//:/\\n}" # Print each PATH entry on a separate line
-alias sym="symfony"
-alias symc="symfony console"
+
+alias cp="cp -ir"
+alias mv="mv -iv"
+alias mkdir="mkdir -vp"
 
 # Docker
 alias dc="docker compose"
 alias dcu="docker-compose up"
 alias dcd="docker-compose down"
+
+# PHP
+alias compo="composer"
+alias sym="symfony"
+alias symc="symfony console"
 
 # Specific windows os
 # alias bat="batcat"
