506c26fe75276963d665d5a4f4fc239c094fbd11
Steven DUBOIS <git@duboiss.fr>
2021-10-16 20:18:51 +0200

    mac.sh: add keyboard commands and various

--- scripts/mac.sh
+++ scripts/mac.sh
@@ -5,12 +5,14 @@ if [ -z "$ZSH_VERSION" ]; then
   exec zsh "$0" "$@"
 fi
 
-echo "Enable full keyboard access for all controls"
-# (e.g. enable Tab in modal dialogs)
-defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+echo "Dialog: disable the “Are you sure you want to open this application?”"
+defaults write com.apple.LaunchServices LSQuarantine -bool false
 
 echo "Dock: show indicator lights for open applications"
 defaults write com.apple.dock show-process-indicators -bool true
+
+echo "Dock: hide 'recent applications'"
+defaults write com.apple.dock show-recents -bool false
 
 echo "Dock: change size"
 defaults write com.apple.dock tilesize -int 42
@@ -36,6 +38,20 @@ defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 echo "Finder: set default location to Home"
 defaults write com.apple.finder NewWindowTarget -string "PfLo" && \
 defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}"
+
+echo "Keyboard: enable full keyboard access for all controls"
+# (e.g. enable Tab in modal dialogs)
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+echo "Keyboard: disabling smart quotes and dashes"
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+echo "Keyboard: blazingly fast keyboard repeat rate"
+defaults write NSGlobalDomain KeyRepeat -int 1
+
+echo "Keyboard: shorter delay until key repeat"
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
 
 killall Dock
 killall Finder
