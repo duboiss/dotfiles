6e5668d0d3c42bc40d615430c8114d862c03b2b7
Steven DUBOIS <git@duboiss.fr>
2022-03-09 13:57:51 +0100

    Mac core: use Cloudflare dns servers

--- scripts/mac_configure_core.sh
+++ scripts/mac_configure_core.sh
@@ -7,6 +7,11 @@ echo "Files: avoid creating .DS_Store files on network or USB volumes"
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 
+# System Preferences > Network > Wi-Fi > Advanced > DNS
+# https://www.dnsperf.com/
+echo "Network: use Cloudflare DNS servers for 'Wi-Fi' network service"
+networksetup -setdnsservers Wi-Fi 1.1.1.1 1.0.0.1 2606:4700:4700::1111 2606:4700:4700::1001
+
 echo "Printer: automatically quit printer app once the print jobs complete"
 defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
 
