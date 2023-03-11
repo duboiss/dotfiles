5794869e64ea7cb0f58a7549bccc746786a652b6
Steven DUBOIS <git@duboiss.fr>
2023-03-11 16:30:50 +0100

    Exports: use composer command to find global find dir

--- config/.shell/.exports.zsh
+++ config/.shell/.exports.zsh
@@ -10,7 +10,7 @@ export VOLTA_HOME="$HOME/.volta"
 export PATH="$VOLTA_HOME/bin:$PATH"
 
 # Add global composer and yarn packages to path
-export PATH="$HOME/.composer/vendor/bin:$PATH"
+export PATH="$(composer global config bin-dir --absolute --quiet):$PATH"
 export PATH="$(yarn global bin):$PATH"
 
 # Add local vendor bin packages to path (PHP projects)
