b4be7be9e0ccddcec6757ad7a769aa346d916302
Steven DUBOIS <git@duboiss.fr>
2023-05-10 13:58:24 +0200

    Scripts: add ubuntu/deban boot

--- scripts/boot.sh
+++ scripts/boot.sh
@@ -6,10 +6,23 @@ sudo -v
 # Remove last login text on shell startup
 touch .hushlogin
 
-# Install Homebrew if not already installed
-command -v brew > /dev/null || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
-eval "$(/opt/homebrew/bin/brew shellenv)"
+if [[ $(uname) == "Darwin" ]]; then
+    # Install Homebrew if not already installed (for MacOS)
+    command -v brew > /dev/null || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 
-# Same for oh-my-zsh
-command -v omz > /dev/null || sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    # Install oh-my-zsh if not already installed
+    command -v omz > /dev/null || sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+if [[ $(uname) == "Linux" && -f /etc/debian_version ]]; then
+    # Upgrade packages
+    sudo apt update && sudo apt upgrade -y
+
+    # Install zsh and set it as default shell
+    sudo apt install zsh -y
+    chsh -s $(which zsh)
+    touch .zshrc
+    exec zsh
+fi
