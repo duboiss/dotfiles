ff2b24ccfdc68f4c702781671b12ef794cb6c2bb
Steven DUBOIS <git@duboiss.fr>
2024-11-23 20:08:34 +0100

    Windows: remove terminal preview and add libreoffice/tableplus

--- windows/winget.bat
+++ windows/winget.bat
@@ -1,35 +1,36 @@
-winget install --id=Google.Chrome -e 
-&& winget install --id=Microsoft.WindowsTerminalPreview -e 
-&& winget install --id=AnyDeskSoftwareGmbH.AnyDesk -e 
-&& winget install --id=NickeManarin.ScreenToGif -e 
-&& winget install --id=Mega.MEGASync -e 
-&& winget install --id=Notion.Notion -e 
-&& winget install --id=QL-Win.QuickLook  -e 
-&& winget install --id=Piriform.Speccy -e 
-&& winget install --id=Microsoft.PowerToys -e 
-&& winget install --id zyedidia.micro -e 
+winget install --id Google.Chrome -e 
+&& winget install --id AnyDeskSoftwareGmbH.AnyDesk -e 
+&& winget install --id NickeManarin.ScreenToGif -e 
+&& winget install --id Mega.MEGASync -e 
+&& winget install --id Notion.Notion -e 
+&& winget install --id QL-Win.QuickLook  -e 
+&& winget install --id Piriform.Speccy -e 
+&& winget install --id Microsoft.PowerToys -e 
 
 # Communication
-&& winget install --id=Discord.Discord -e 
-&& winget install --id=SlackTechnologies.Slack -e 
+&& winget install --id Discord.Discord -e 
+&& winget install --id SlackTechnologies.Slack -e 
 
 # Development
-&& winget install --id Microsoft.Powershell -e 
-&& winget install --id=Docker.DockerDesktop -e 
-&& winget install --id=Insomnia.Insomnia -e 
-&& winget install --id=JetBrains.Toolbox -e 
-&& winget install --id=Microsoft.VisualStudioCode -e 
+&& winget install --id Microsoft.PowerShell -e 
+&& winget install --id Docker.DockerDesktop -e 
+&& winget install --id Insomnia.Insomnia -e 
+&& winget install --id JetBrains.Toolbox -e 
+&& winget install --id zyedidia.micro -e 
+&& winget install --id Microsoft.VisualStudioCode -e 
+&& winget install --id TablePlus.TablePlus -e 
 && winget install --id Volta.Volta -e 
 
 # Files
-&& winget install --id=SumatraPDF.SumatraPDF -e 
-&& winget install --id=PDFsam.PDFsam -e 
-&& winget install --id=VideoLAN.VLC -e 
-&& winget install --id=WebTorrent.Desktop -e 
-&& winget install --id=RARLab.WinRAR -e 
+&& winget install --id TheDocumentFoundation.LibreOffice -e 
+&& winget install --id SumatraPDF.SumatraPDF -e 
+&& winget install --id PDFsam.PDFsam -e 
+&& winget install --id VideoLAN.VLC -e 
+&& winget install --id WebTorrent.Desktop -e 
+&& winget install --id RARLab.WinRAR -e 
 
 # Games
-&& winget install --id=EpicGames.EpicGamesLauncher -e 
-&& winget install --id=Ubisoft.Connect -e 
-&& winget install --id=Valve.Steam -e 
-&& winget install --id=ElectronicArts.EADesktop -e
+&& winget install --id EpicGames.EpicGamesLauncher -e 
+&& winget install --id Ubisoft.Connect -e 
+&& winget install --id Valve.Steam -e 
+&& winget install --id ElectronicArts.EADesktop -e
